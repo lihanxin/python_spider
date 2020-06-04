182ec7f4d758a633414ae3b1642a5a55125de03e
li_han_xin <18898601264@163.com>
2020-06-04 15:51:19 +0800

    git branch  push

--- crawler/test/baidu/baidu/settings.py
+++ crawler/test/baidu/baidu/settings.py
@@ -20,7 +20,7 @@ NEWSPIDER_MODULE = 'baidu.spiders'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = False
-#test
+#LOG_LEVEL
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
 
